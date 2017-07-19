3c15318b88ce045178fc7d2412db377125b978f1
Nirui <nirui@lianjia.com>
2017-07-19 17:33:05 +0800

    Fix bug

--- cycleviewpager_library/src/main/java/com/cycleviewpager/rainbow/cycleviewpager_library/CycleViewPager.java
+++ cycleviewpager_library/src/main/java/com/cycleviewpager/rainbow/cycleviewpager_library/CycleViewPager.java
@@ -3212,7 +3212,7 @@ public class CycleViewPager extends ViewGroup {
 
     private int getRealPosition(int position, int totalCount) {
         if (position < 0) {
-            return position % totalCount + totalCount;
+            return (position + totalCount) % totalCount;
         } else {
             return position % totalCount;
         }
